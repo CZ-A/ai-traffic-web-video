0d1683a6e2e783d953a5bb263d92502f8918902d
CZ-A <adrianfalsha@gmail.com>
2025-01-10 19:29:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -74,6 +74,9 @@ def generate_traffic():
     if type_traffic == "web":
         options = Options()
         options.add_argument("--headless")  # Menggunakan mode headless
+        options.add_argument('--disable-gpu')  # Menghindari masalah dengan GPU di beberapa sistem
+        options.add_argument('--no-sandbox')  # Menjalankan di lingkungan aman
+        options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')  # User-Agent
         service = Service("./chromedriver/chromedriver")
         driver = webdriver.Chrome(service=service, options=options)
 
@@ -102,6 +105,9 @@ def generate_traffic():
     elif type_traffic == "video":
         options = Options()
         options.add_argument("--headless")  # Menggunakan mode headless
+        options.add_argument('--disable-gpu')  # Menghindari masalah dengan GPU di beberapa sistem
+        options.add_argument('--no-sandbox')  # Menjalankan di lingkungan aman
+        options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')  # User-Agent
         service = Service("./chromedriver/chromedriver")
         driver = webdriver.Chrome(service=service, options=options)
 
